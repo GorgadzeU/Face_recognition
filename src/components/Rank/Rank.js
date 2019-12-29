2771be83f6c304dada534655e4c35135f8b480ed
gorgadzeU <gorgadzeu@gmail.com>
2019-12-29 16:45:25 +0400

    'NR'

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return(
+        <div>
+            <div className='center white f3'>
+                {`Your current rank is....`}
+            </div>
+            <div className='white f1 center '>
+                {`5`}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
